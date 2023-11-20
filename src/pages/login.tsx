38467af26fc3850b1777e11fafc634b8c06dc058
lee88688 <lee88688@163.com>
2023-11-20 09:31:57 +0000

    update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,8 +4,6 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import { useFormik } from "formik";
 import { apiLogin } from "./clientApi";
 import { toFormikValidationSchema } from "zod-formik-adapter";
@@ -32,60 +30,64 @@ export default function Login() {
   });
 
   return (
-    <Container component="main" maxWidth="xs">
-      <div className="flex flex-col items-center">
-        <Typography component="h1" variant="h5">
-          用户登陆
-        </Typography>
-        <form className="mt-2 w-full" noValidate>
-          <TextField
-            value={formik.values.username}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={Boolean(formik.errors.username) && formik.touched.username}
-            helperText={formik.touched.username && formik.errors.username}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="username"
-            label="用户名"
-            autoFocus
-            disabled={formik.isSubmitting}
-          />
-          <TextField
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={Boolean(formik.errors.password) && formik.touched.password}
-            helperText={formik.touched.password && formik.errors.password}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="密码"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            disabled={formik.isSubmitting}
-          />
-          {/* <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          /> */}
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2 }}
-            disabled={!formik.isValid}
-            onClick={() => formik.handleSubmit()}
-          >
-            登 陆
-          </Button>
-        </form>
-      </div>
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "20vh",
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        用户登陆
+      </Typography>
+      <form className="mt-2 w-full" noValidate>
+        <TextField
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={Boolean(formik.errors.username) && formik.touched.username}
+          helperText={formik.touched.username && formik.errors.username}
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="username"
+          label="用户名"
+          autoFocus
+          disabled={formik.isSubmitting}
+        />
+        <TextField
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={Boolean(formik.errors.password) && formik.touched.password}
+          helperText={formik.touched.password && formik.errors.password}
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="密码"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          disabled={formik.isSubmitting}
+        />
+        <Button
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          disabled={!formik.isValid}
+          onClick={() => formik.handleSubmit()}
+        >
+          登 陆
+        </Button>
+      </form>
     </Container>
   );
 }
